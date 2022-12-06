215f96c5392ec3a66b2645d78c16c81b04eb091a
WebDev-Chad <chadwin.allotey@gmail.com>
2022-12-05 19:59:28 -0500

    update

---
+++ src/components/common/header/Header.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header = () => {
+  return <div style={{zIndex:10}}>Header</div>;
+};
+
+export default Header;
---
+++ src/components/home/carousel/Carousel.tsx
@@ -0,0 +1,42 @@
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import { AutoScroll } from "@splidejs/splide-extension-auto-scroll";
+import "@splidejs/splide/dist/css/splide.min.css";
+
+const Carousel = () => {
+  const renderSlider = () => {
+    const results = [];
+    for (let i = 1; i < 7; i++) {
+      results.push(
+        <SplideSlide key={i} >
+          <img src={require(`../../images/hero0${i}_en.jpg`)} alt="Image 1" />
+        </SplideSlide>
+      );
+    }
+    return results;
+  };
+
+  return (
+    <Splide
+      options={{
+        type: "loop",
+        gap: "10",
+        drag: false,
+        arrows: false,
+        pagination: false,
+        perPage: 4,
+
+        autoScroll: {
+          pauseOnHover: false,
+          pauseOnFocus: false,
+          rewind: false,
+          speed: 0.18,
+        },
+      }}
+      extensions={{ AutoScroll }}
+    >
+      {renderSlider()}
+    </Splide>
+  );
+};
+
+export default Carousel;
