bf8978ad637004ce4404ac757a5c532e5add3bec
WebDev-Chad <chadwin.allotey@gmail.com>
2022-12-05 20:59:51 -0500

    update

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -1,7 +1,37 @@
 import React from "react";
 
 const Header = () => {
-  return <div style={{zIndex:10}}>Header</div>;
+  return (
+    <>
+      <div
+        style={{
+          position: "absolute",
+          top: "0px",
+          left: "0px",
+        }}
+      >
+        <img
+          src={require(`../../images/Nintendo.png`)}
+          style={{ height: "3rem", width: "7.68rem" }}
+          alt=""
+        />
+      </div>
+
+      <div
+        style={{
+          position: "absolute",
+          top: "0px",
+          right: "0px",
+        }}
+      >
+        <img
+          src={require(`../../images/NintendoSwitchLogo.png`)}
+          style={{ height: "5.2rem", width: "4.68rem" }}
+          alt=""
+        />
+      </div>
+    </>
+  );
 };
 
 export default Header;
--- src/components/home/carousel/Carousel.tsx
+++ src/components/home/carousel/Carousel.tsx
@@ -1,13 +1,14 @@
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 import { AutoScroll } from "@splidejs/splide-extension-auto-scroll";
 import "@splidejs/splide/dist/css/splide.min.css";
+import Header from "../../common/header/Header";
 
 const Carousel = () => {
   const renderSlider = () => {
     const results = [];
     for (let i = 1; i < 7; i++) {
       results.push(
-        <SplideSlide key={i} >
+        <SplideSlide key={i}>
           <img src={require(`../../images/hero0${i}_en.jpg`)} alt="Image 1" />
         </SplideSlide>
       );
@@ -16,26 +17,29 @@ const Carousel = () => {
   };
 
   return (
-    <Splide
-      options={{
-        type: "loop",
-        gap: "10",
-        drag: false,
-        arrows: false,
-        pagination: false,
-        perPage: 4,
+    <>
+      <Splide
+        options={{
+          type: "loop",
+          gap: "10",
+          drag: false,
+          arrows: false,
+          pagination: false,
+          perPage: 4,
 
-        autoScroll: {
-          pauseOnHover: false,
-          pauseOnFocus: false,
-          rewind: false,
-          speed: 0.18,
-        },
-      }}
-      extensions={{ AutoScroll }}
-    >
-      {renderSlider()}
-    </Splide>
+          autoScroll: {
+            pauseOnHover: false,
+            pauseOnFocus: false,
+            rewind: false,
+            speed: 0.18,
+          },
+        }}
+        extensions={{ AutoScroll }}
+      >
+        {renderSlider()}
+      </Splide>
+      <Header />
+    </>
   );
 };
 
